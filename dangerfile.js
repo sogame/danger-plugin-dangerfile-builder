74b1ccbfa94df266f99c9d528305ba2eeac52ae8
Jaume Salgado <jaume.salgado@skyscanner.net>
2018-10-10 22:34:04 +0200

    Enable Danger in Travis

--- dangerfile.js
+++ dangerfile.js
@@ -1,3 +1,29 @@
-// const { commonChangelog } = require('danger-plugin-dangerfile-builder'); // eslint-disable-line import/no-extraneous-dependencies
+const {
+  commonPrDescriptionContribution,
+  commonContributingGuide,
+  commonChangelog,
+  commonValidJson,
+  jsConsoleCommands,
+  jsGlobalEslintChange,
+  jsLocalEslintChange,
+  jsLockfile,
+  jsTestShortcuts,
+} = require('danger-plugin-dangerfile-builder'); // eslint-disable-line import/no-unresolved, import/no-extraneous-dependencies
 
-// commonChangelog();
+commonPrDescriptionContribution();
+
+commonContributingGuide();
+
+commonChangelog();
+
+commonValidJson();
+
+jsConsoleCommands();
+
+jsGlobalEslintChange();
+
+jsLocalEslintChange();
+
+jsLockfile();
+
+jsTestShortcuts();
